5d6ad937ded5c03d7eaa346717c99217673be075
Cris <criscianysilva1997@gmail.com>
2021-11-28 08:35:39 -0400

    primeiro comentário

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '65214' ,
+                nome: 'Fulano de Tal',
+                partido: 'ABC' ,
+                fotos: [
+                    {url:'pica-pau.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '62541' ,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG' ,
+                fotos: [
+                    {url:'Lula-molusco.jpg', legenda: 'Vereador'}
+                ]
+            },
+        
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: '2',
+        candidatos: [
+            {
+                numero: '41' ,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG' ,
+                vice: 'Cic',
+                fotos: [
+                    {url:'Homer-Simpson.jpg', legenda: 'Prefeito'},
+                    {url:'Gumball.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '42' ,
+                nome: 'Beltrano de Souza',
+                partido: 'HIJK' ,
+                vice: 'Circo',
+                fotos: [
+                    {url:'ioda.png', legenda: 'Prefeito'},
+                    {url:'popeye.jpg', legenda: 'Vice-Prefeito', small:true}
+                ] 
+            },
+        ]
+    }
+];
